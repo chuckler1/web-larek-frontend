23a486864072f7637d0bc4ea1b0bfa9cd4a871e2
Denis Kozlov <denisdesdoz@yandex.ru>
2024-02-04 16:55:40 +0300

    На ревью2

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
-export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
+export const API_URL = `${process.env.API_ORIGIN}/api/weblarek` || 'https://larek-api.nomoreparties.co/api/weblarek';
+export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek` || 'https://larek-api.nomoreparties.co/content/weblarek';
